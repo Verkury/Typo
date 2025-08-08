5c5219f6c7349a56314af4dbb8db1c6d31d6256f
Ver <aleksandrlaty78@gmail.com>
2025-08-08 12:09:58 +0700

    Add support text for json and recode for c++

--- Typo/Typo.cpp
+++ Typo/Typo.cpp
@@ -1,29 +1,55 @@
 ﻿#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <random>
+#include <json.hpp>
+
+using namespace std;
+using json = nlohmann::json;
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Ошибка: Укажите файл. Пример: typo file.txt\n");
-        return 1;
-    }
-    
-    if (argc == 1) {
-        FILE* file = fopen(argv[1], "r");
-        if (!file) {
-            perror("Ошибка открытия файла");
+    // Проверяем, передан ли аргумент (имя файла)
+    if (argc == 2) {
+        const string filename = argv[1];
+        ifstream file(filename);
+
+        if (!file.is_open()) {
+            cerr << "Error: Could not open file '" << filename << "'\n";
             return 1;
         }
 
-        char line[256];
-        while (fgets(line, sizeof(line), file)) {
-            printf("%s", line);
+        // Просто выводим содержимое файла (аналог `cat`)
+        string line;
+        while (getline(file, line)) {
+            cout << line << "\n";
+        }
+        return 0;
+        
+    }
+    else if (argc == 1) 
+    {
+        ifstream file("Texts.json");
+
+        if (!file.is_open()) {
+            cerr << "Не удалось открыть файл!\n";
+            return 1;
         }
 
-        fclose(file);
-        return 0;
-    }
-    else {
+        json data;
+        file >> data;
 
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> distrib(0, 20);
+        int index = distrib(gen);
+
+        cout << data[to_string(index)];
+
+
+    }
+    
+    else {
+        cerr << "Usage: " << argv[0] << " <filename>\n";
+        return 1;
     }
 }
